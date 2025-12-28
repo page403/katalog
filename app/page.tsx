55cbedc36fbccc82a70831fdc76d44c97e9a9ac4
page403 <studioswahid@gmail.com>
2025-12-28 16:54:06 +0700

    fix not show data

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import { getProducts } from '@/lib/storage';
 import CatalogPage from '@/components/CatalogPage';
 import { getCategories, getTags, getSuppliers } from '@/lib/storage';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
   const products = await getProducts();
   const [categories, tags, suppliers] = await Promise.all([getCategories(), getTags(), getSuppliers()]);
